d9e1202b371fae2fc5bd0e1d64f5d94662acde72
radshima <shima.akbarirad@gmail.com>
2023-10-18 20:20:37 +1100

    local storage added

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+window.addEventListener('load',showTasks);
+ 
 let addBtn = document.querySelector('button');
 let taskList = document.querySelector('ul');
 let input = document.querySelector('input');
-
+let tasks;
+if (!localStorage.getItem('todo')){
+    tasks = [];
+}else{
+tasks = getTasks();
+}
 
 addBtn.addEventListener('click' , ()=>{
     let text = input.value;
@@ -9,15 +16,19 @@ addBtn.addEventListener('click' , ()=>{
         let task = createTask(text);
         task.innerHTML += '<span class="closeBtn"><i class="fa-solid fa-trash"></i></span>'
         taskList.appendChild(task);
+        saveTasks(text);
         input.value = "";
     }
 });
 
 
-
 taskList.addEventListener('click', (e)=>{
     if (e.target.nodeName === 'I'){
-    e.target.parentElement.parentElement.style = 'display : none';
+    let target = e.target.parentElement.parentElement;
+    target.style = 'display : none';
+    tasks.splice(tasks.indexOf(target.textContent), 1 );
+    localStorage.setItem('todo',tasks);
+
     }
     if (e.target.nodeName === 'LI') {
         e.target.classList.toggle("done"),
@@ -31,14 +42,22 @@ function createTask(text){
     return li;
 }
 
+function saveTasks (text) {
+    tasks.push(text);
+    localStorage.setItem('todo', tasks);
+}
 
-
-
-
-
-
-
-
+function getTasks(){
+    return localStorage.getItem('todo').split(',');
+}
+ 
+function showTasks(){
+for (let taskText of getTasks()) {
+    let task = createTask(taskText);
+task.innerHTML += '<span class="closeBtn"><i class="fa-solid fa-trash"></i></span>'
+taskList.appendChild(task);
+}
+}
 
 
 
